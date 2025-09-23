90d1d7ddb7eebfeae258841a09ca907178ae0d6e
silverwind <me@silverwind.io>
2025-09-23 19:07:06 +0200

    updates

--- bench.ts
+++ bench.ts
@@ -1,8 +1,8 @@
 import {parseIp, stringifyIp} from "./index.ts";
 
-let t;
-const ip4s = [];
-const ip6s = [];
+let t: DOMHighResTimeStamp;
+const ip4s: Array<string> = [];
+const ip6s: Array<string> = [];
 
 t = performance.now();
 for (let i = 0; i < 1e5; i++) ip4s.push(stringifyIp({number: BigInt(i), version: 4}));
